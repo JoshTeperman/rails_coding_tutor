5c0a50a64f46940a718d3ee4b6cd900e261726e5
Chen Hong Liang <chenhongliang@Chens-MacBook-Pro-3.local>
2019-04-30 11:15:45 +1000

    created profile model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/', to: 'profiles#index', as: 'home'
+  root to: 'profiles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
