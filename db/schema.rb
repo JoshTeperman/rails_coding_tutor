6cb03620eae28ff57fb4bc9fbfb1727cb112df5f
Josh Teperman <joshteperman@gmail.com>
2019-05-06 12:37:55 +1000

    reinstalled active_storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_013606) do
+ActiveRecord::Schema.define(version: 2019_05_06_023130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
