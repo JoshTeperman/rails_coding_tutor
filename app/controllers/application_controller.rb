05615dcd19752fbe1608d5a951ddec2ca61f0d4b
Josh Teperman <joshteperman@gmail.com>
2019-05-07 16:18:48 +1000

    footer partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :has_profile?
-  helper_method :profile_is_admin?
 
-  rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
-      format.html { redirect_to main_app.root_url, notice: exception.message }
-      format.js   { head :forbidden, content_type: 'text/html' }
-    end
-  end
-
-  # def require_user
-  #   redirect_to '/login' unless current_user
-  # end
-  # def require_profile
-  #   if signed_in?
-  #     redirect_to new_profile_path unless current_user.profile
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   respond_to do |format|
+  #     format.json { head :forbidden, content_type: 'text/html' }
+  #     format.html { redirect_to main_app.root_url, notice: exception.message }
+  #     format.js   { head :forbidden, content_type: 'text/html' }
   #   end
   # end
+
 
   private
     def has_profile?
